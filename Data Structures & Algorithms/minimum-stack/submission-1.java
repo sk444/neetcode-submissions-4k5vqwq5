7761d208d1a95ebcd9f9fd998eb15283b41efe3c
sk444 <46106303+sk444@users.noreply.github.com>
2026-04-07 08:48:59 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,38 @@
+class MinStack 
+{
+    LinkedList<Integer> stack = new LinkedList<>();
+    LinkedList<Integer> minStack = new LinkedList<>();
+
+    public MinStack() {
+    }
+    
+    public void push(int val)
+    {
+        stack.addFirst(val);
+
+        if(minStack.isEmpty() || val <= minStack.getFirst())
+        {
+            minStack.addFirst(val);
+        }  
+    }
+    
+    public void pop()
+    {
+        if(stack.isEmpty()) return;
+        if(stack.getFirst().equals(minStack.getFirst()))
+        {
+         minStack.removeFirst();
+        }
+        stack.removeFirst();
+    }
+    
+    public int top() 
+    {
+        return stack.getFirst();
+    }
+    
+    public int getMin() 
+    {
+      return minStack.getFirst();  
+    }
+}
