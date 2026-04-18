9901796ddd68f213ed6202376f632e9339ed45a7
sk444 <46106303+sk444@users.noreply.github.com>
2026-04-18 19:26:30 +0530

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution 
+{
+    public boolean isBalanced(TreeNode root)
+    {
+        if(inorder(root) == -1) return false;
+        return true;
+        
+    }
+
+    private int inorder(TreeNode node)
+    {
+        if(node == null)
+        {
+            return 0;
+        }
+
+        int left =  inorder(node.left);
+
+        if(left == -1) return -1;
+
+        int right = inorder(node.right);
+
+        if(right == -1) return -1;
+
+        if(left > right && left-right > 1) 
+        {
+            return -1;
+        }
+        else if(left < right && right - left > 1)
+        {
+             return -1;
+        }
+
+        return 1 + Math.max(left, right);
+    }
+}
