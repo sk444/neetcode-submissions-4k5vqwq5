e12ed097a95b917281c3ce61214d472fd75f9ad7
sk444 <46106303+sk444@users.noreply.github.com>
2026-04-28 20:54:58 +0530

    Add: last-stone-weight - submission-6

---
+++ Data Structures & Algorithms/last-stone-weight/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lastStoneWeight(int[] arr)
+    {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b-a);
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            pq.offer(arr[i]);
+
+        }
+         while(pq.size() > 1)
+         {
+               int x = pq.poll();
+               int y = pq.poll();
+
+              pq.offer(x - y);
+
+            
+         }
+         return pq.size() == 0 ? 0: pq.peek();
+        
+    }
+}
