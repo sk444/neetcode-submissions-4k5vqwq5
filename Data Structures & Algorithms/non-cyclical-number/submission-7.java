1111e5a4925bdd7b225bf46589cd11799b76e0e3
sk444 <46106303+sk444@users.noreply.github.com>
2026-04-29 20:39:34 +0530

    Add: non-cyclical-number - submission-7

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-7.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isHappy(int n) 
+    {
+        Set<Integer> set = new HashSet<>();
+        
+        while(n != 1 && !set.contains(n))
+        {
+           set.add(n);
+
+           n = checkHappy(n);
+        }
+          return n == 1;
+    }
+       
+    
+
+    private int checkHappy(int n)
+    {
+        int sum = 0;
+
+        while(n != 0)
+        {
+            int digit = n % 10;
+
+            sum = sum + (digit * digit) ;
+            
+            n = n/10;
+        }
+        return sum;
+    }
+}
