c7faa7e1cda15ffcca747096cd591ee2a80f4d80
sk444 <46106303+sk444@users.noreply.github.com>
2026-07-07 19:58:20 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) 
+    {
+      char[] ch1 = s.toCharArray();
+      char[] ch2 = t.toCharArray();
+
+      Arrays.sort(ch1);
+      Arrays.sort(ch2);
+
+      String s1 = new String(ch1);
+      String s2 = new String(ch2);
+
+      return s1.equals(s2);
+
+    }
+}
